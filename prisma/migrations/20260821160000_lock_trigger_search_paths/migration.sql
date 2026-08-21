da472aabe407e50619334852efb973445062748c
AstralEX1 <119760163+AstralEX1@users.noreply.github.com>
2026-08-21 14:07:50 +0400

    fix(db): lock trigger function search paths

---
+++ prisma/migrations/20260821160000_lock_trigger_search_paths/migration.sql
@@ -0,0 +1,4 @@
+ALTER FUNCTION prevent_mineral_economy_cutover_mutation() SET search_path = '';
+ALTER FUNCTION prevent_planet_upgrade_purchase_mutation() SET search_path = '';
+ALTER FUNCTION prevent_mineral_account_identity_mutation() SET search_path = '';
+ALTER FUNCTION prevent_ready_backend_planet_mutation() SET search_path = '';
